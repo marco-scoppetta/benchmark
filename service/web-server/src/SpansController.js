4f3e7340e65ae5e37b29151b62ecb9c45319b074
Marco Scoppetta <marco@grakn.ai>
2019-03-21 18:49:58 +0000

    added executionSpans handling in SpansController

--- service/web-server/src/SpansController.js
+++ service/web-server/src/SpansController.js
@@ -8,35 +8,55 @@ function SpansController(esClient){
 
 const typeDefs = `
   type Query {
-    querySpans(graphName: String,
-        executionName: String,
-        orderBy: String, 
-        order: String, 
+    querySpans(
+        parentId: String,
         offset: Int, 
-        limit: Int): [Span]
+        limit: Int): [QuerySpan]
+
+    executionSpans(
+        executionName: String,
+        graphTypeName: String
+    ): [ExecutionSpan]
   }
 
-  type Span {
+  type ExecutionSpan {
     id: String!
     duration: Int!
     name: String!
-    tags: Tag
+    tags: ExecutionSpanTag
   }
 
-  type Tag {
+  type ExecutionSpanTag {
+    configurationName: String,
+    description: String,
     executionName: String,
-    graphName: String,
+    concurrentClient: Int,
+    graphGeneratorDefinition: String,
+    queryRepetitions: Int,
+    graphScale: Int
+  }
+
+  type QuerySpan {
+    id: String!
+    parentId: String
+    duration: Int!
+    name: String!
+    tags: QuerySpanTag
+  }
+
+  type QuerySpanTag {
+    type: String,
     query: String,
     repetition: Int,
-    repetitions: Int,
-    scale: Int
+    repetitions: Int
   }
 `;
 
-function filterQuerySpans(args){
-    const must = [{ match: { name: "query"} } ];
-    if(args.graphName) {
-        must.push({ match: { "tags.graphName": args.graphName }});
+
+function filterExecutionSpans(args){
+    const must = [{ match: { name: "concurrent-execution"} } ];
+    if(args.graphGeneratorDefinition) {
+        must.push({ match: { "tags.graphGeneratorDefinition": args.graphGeneratorDefinition }});
     }
     if(args.executionName) {
         must.push({ match: { "tags.executionName": args.executionName }});
@@ -48,12 +68,26 @@ function limitQuery(offset, limit){
     return {from : offset || 0 , size : limit || 50};
 }
 
+function filterQuerySpans(args){
+    const must = [{ match: { name: "query"} } ];
+    if(args.parentId) {
+        must.push({ match: { "parentId": args.parentId }});
+    }
+    return { query: { bool: { must } } };
+}
+
 const resolvers = {
     Query: {
         querySpans: (object, args, context, info) => {
             let body = {};
             Object.assign(body, limitQuery(args.offset, args.limit));
             Object.assign(body, filterQuerySpans(args));
+            const queryObject = Object.assign({index: "benchmark*", type: "span"}, { body });
+            return context.client.search(queryObject).then(result => result.hits.hits.map(res => res._source));
+        },
+        executionSpans: (object, args, context, info) => {
+            let body = {};
+            Object.assign(body, filterExecutionSpans(args));
             const queryObject = Object.assign({index: "benchmark*", type: "span"}, { body });
             return context.client.search(queryObject).then(result => result.hits.hits.map(res => res._source));
         }
